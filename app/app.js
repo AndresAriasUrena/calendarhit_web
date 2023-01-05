0b0b9a16abf21dea2ac944fa5812cc12da7c64a1
Andres Arias <andres.arur@outlook.com>
2023-01-05 11:28:51 -0600

    styling cta buttons plus adding media querys for nav bar

--- app/app.js
+++ app/app.js
@@ -1,7 +1,20 @@
-var navLinks = document.getElementById("navLinks");
-function showMenu(){
-    navLinks.style.right = "0";
-    }
-function hideMenu(){
-    navLinks.style.right = "-200px";
+// var navLinks = document.getElementById("navLinks");
+// function showMenu(){
+//     navLinks.style.right = "0";
+//     }
+// function hideMenu(){
+//     navLinks.style.right = "-200px";
+// }
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle');
 }
